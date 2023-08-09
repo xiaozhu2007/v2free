30fba6c3af0b3e37839f5fbcf9b5746a02133cb0
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-08-09 18:20:15 +0800

    Update check_in.py

--- check_in.py
+++ check_in.py
@@ -5,7 +5,6 @@ import logging
 import argparse
 import requests
 
-
 class CheckIn(object):
     client = requests.Session()
     login_url = "https://w1.v2free.top/auth/login"
@@ -36,24 +35,22 @@ class CheckIn(object):
             "code": "",
         }
         self.client.post(self.login_url, data=data, headers=headers)
-
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
             "Referer": "https://w1.v2free.top/user",
         }
         response = self.client.post(self.sign_url, headers=headers)
-
         logging.info(self.masked_username + "\t" + response.json()["msg"])
 
 
 if __name__ == "__main__":
-    LOG_FORMAT = "%(asctime)s\t%(levelname)s\t%(message)s"
+    LOG_FORMAT = "[%(asctime)s] [%(levelname)s] %(message)s"
     logging.basicConfig(filename='run.log',
                         level=logging.INFO, format=LOG_FORMAT)
 
-    parser = argparse.ArgumentParser(description='V2free 签到脚本')
-    parser.add_argument('--username', type=str, help='您的账号')
-    parser.add_argument('--password', type=str, help='您的密码')
+    parser = argparse.ArgumentParser(description='V2free 自动签到脚本')
+    parser.add_argument('--username', type=str, help='您的账号(仅支持单个)')
+    parser.add_argument('--password', type=str, help='您的密码(仅支持单个)')
     args = parser.parse_args()
     helper = CheckIn(args.username, args.password)
     helper.check_in()
