5cb5b1ce722ea11577cc27f9c019af47f4227393
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-10-25 20:45:50 +0800

    Update check_in.py

--- check_in.py
+++ check_in.py
@@ -54,7 +54,7 @@ class CheckIn(object):
         ##### DEBUG #####
         print(response.text)
         ##### DEBUG #####
-        # logging.info(self.masked_username + " " + response.json()["msg"])
+        logging.info(self.masked_username + " " + "获得了 未知 流量.") # response.json()["msg"]
 
 
 if __name__ == "__main__":
